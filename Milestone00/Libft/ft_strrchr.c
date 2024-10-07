91d9ab99e72ae8c304a314467518ada2f6ecbf4a
Roberto Muñoz Soler <roberto@MacBook-Air-de-Roberto.local>
2024-10-07 10:12:58 +0200

    Corregido

--- Milestone00/Libft/ft_strrchr.c
+++ Milestone00/Libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rmunoz-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 10:43:40 by rmunoz-s          #+#    #+#             */
-/*   Updated: 2024/10/04 10:09:57 by rmunoz-s         ###   ########.fr       */
+/*   Updated: 2024/10/04 10:37:35 by rmunoz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,14 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char		*str;
-	char		find_c;
-	size_t		i;
+	int	i;
 
-	str = (char *)s;
-	find_c = (char)c;
 	i = ft_strlen(s);
-	while (i > 0)
+	while (i >= 0)
 	{
-		if (str[i] == find_c)
-			return (str + i);
+		if (s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
 		i--;
 	}
-	if (str[0] == find_c)
-		return (str);
-	return (NULL);
+	return (0);
 }
